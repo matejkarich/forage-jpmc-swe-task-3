da0ffd725c70fb0ef226ea15f00ab62465230731
Richard Matejka <matejkarich@gmail.com>
2023-07-22 11:21:09 -0700

    Modify component lifecycle method to include TableData and add new respective import

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import {Table, TableData} from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -56,7 +56,7 @@ class Graph extends Component<IProps, {}> {
   componentDidUpdate() {
     if (this.table) {
       this.table.update(
-        DataManipulator.generateRow(this.props.data),
+        [DataManipulator.generateRow(this.props.data),] as unknown as TableData
       );
     }
   }
